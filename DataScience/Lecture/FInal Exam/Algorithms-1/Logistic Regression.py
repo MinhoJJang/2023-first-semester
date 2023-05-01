0940cdff114c986132e20e7d3a297b4f45c5ddd0
MinhoJJang <ahchjang@naver.com>
2023-05-01 11:44:18 +0900

    05/01 Lesson

---
+++ DataScience/Lecture/FInal Exam/Algorithms-1/Logistic Regression.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_digits
+digits = load_digits()
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.25, random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+logisticRegr = LogisticRegression()
+logisticRegr.fit(x_train, y_train)
+
+logisticRegr.predict(x_test[0].reshape(1,-1))
+logisticRegr.predict(x_test[0:10])
+predictions = logisticRegr.predict(x_test)
+
+score = logisticRegr.score(x_test, y_test)
+print(score)
